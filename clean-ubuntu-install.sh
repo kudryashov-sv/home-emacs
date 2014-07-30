440ac9f518b966f1ffedfb81fb20a3ff3dc43e5e
Kudryashov Sergey <kudryashov-sv@yandex.ru>
2014-07-30 15:22:15 -0700

    Add 'clean-ubuntu-install.sh'

---
+++ clean-ubuntu-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+pre_install_msg="$HOME/.emacs exists, backup and cleanup $HOME/.emacs, $HOME/.emacs.d"
+
+sudo apt-get update || exit 1
+sudo apt-get install curl git aspell emacs24 -y || exit 1
+
+if [ -f $HOME/.emacs ]; then
+    echo "$pre_install_msg"
+    exit 1
+fi
+
+if [ -d $HOME/.emacs.d ]; then
+    echo "$pre_install_msg"
+fi
+
+echo "Download and install prelude"
+curl -L "https://github.com/bbatsov/prelude/raw/master/utils/installer.sh" | sh || exit 1
+
+echo "Download .emacs file"
+curl -L "https://github.com/kudryashov-sv/home-emacs/raw/master/.emacs" > $HOME/.emacs || exit 1
+
+emacs24 --daemon
